89655f9bdf6662de70f2eb4dc44f26cf94f386be
selah <nsuiivi@gmail.com>
2026-09-03 16:34:50 +0200

    working on tests

--- src/test/java/com/mycompany/hospitalbed/Tests.java
+++ src/test/java/com/mycompany/hospitalbed/Tests.java
@@ -30,8 +30,69 @@ public class Tests {
         return conditions[random.nextInt(conditions.length)];
     }
     
-    Patient a = new Patient(10000 + random.nextInt(89000), 1 + random.nextInt(90), randomName(), randomName(),randomGender(), randomCondition(), PatientCategory.Emergency);
-    Patient b = new Patient(10000 + random.nextInt(89000), 1 + random.nextInt(90), randomName(), randomName(), randomGender(), randomCondition(), PatientCategory.Outpatient);
+    Patient newPatient() {
+        return new Patient(10000 + random.nextInt(89000), 1 + random.nextInt(90), randomName(), randomName(),randomGender(), randomCondition(), PatientCategory.Outpatient);
+    }
+    
     //note: the ward number is 3 due to the scenario so it is hardcoded.
-    Inpatient c = new Inpatient(10000 + random.nextInt(89000), 1 + random.nextInt(90), randomName(), randomName(), randomGender(), randomCondition(), PatientCategory.Inpatient, 3, 1 + random.nextInt(19));
+    Inpatient newInpatient() {
+        return new Inpatient(10000 + random.nextInt(89000), 1 + random.nextInt(90), randomName(), randomName(), randomGender(), randomCondition(), PatientCategory.Inpatient, 3, 1 + random.nextInt(19));
+    }
+    
+    /////////////////////////////////////tests//////////////////////////////////
+    
+    @Test
+    void registerAPatient1() {
+        Patient testPatient = newPatient();
+        Patient.listOfPatients.add(testPatient);
+        assertTrue(Patient.listOfPatients.contains(testPatient));
+    }
+    
+    @Test
+    void registerAPatient2() {
+        Inpatient testInpatient = newInpatient();
+        Inpatient.listOfPatients.add(testInpatient);
+        assertTrue(Inpatient.listOfPatients.contains(testInpatient));
+    }
+    
+    @Test
+    void searchForPatient() {
+        Patient patient1 = newPatient();
+        Patient patient2 = newPatient();
+        Patient.listOfPatients.add(patient1);
+        Patient.listOfPatients.add(patient2);
+        
+        Patient found = null;
+        //(referenced) looping through objects
+        for (Patient patient : Patient.listOfPatients) {
+            if (patient.getPatientID() == patient2.getPatientID()) {
+                found = patient;
+            }
+        }
+        assertEquals(patient2, found);
+    }
+    
+    @Test
+    void UpdatePatientDetails() {
+        Patient patient1 = newPatient();
+        Patient.listOfPatients.add(patient1);
+        
+        patient1.setFirstName("updatedName");
+        patient1.setAge(7);
+        
+        assertEquals("updatedName", patient1.getFirstName());
+        assertEquals(7, patient1.getAge());
+    }
+    
+    @Test
+    void deletePatient() {
+        boolean exists = false;
+        Patient patient1 = newPatient();
+        Patient.listOfPatients.add(patient1);
+        Patient.listOfPatients.remove(patient1);
+        if(Patient.listOfPatients.contains(patient1)) {
+            exists =  true;
+        }
+        assertTrue(exists);
+    }
 }
